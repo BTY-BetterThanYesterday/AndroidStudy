0d08fdc3ca1fbdb5698473fcd92ebfca88e47785
zmfwml77 <zmfwml77@naver.com>
2020-10-31 16:01:53 +0900

    2주차 숙제 제출합니다.

--- yoonwon/app/src/main/java/com/example/bty_homework/MainActivity.kt
+++ yoonwon/app/src/main/java/com/example/bty_homework/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.bty_homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.time.temporal.TemporalAdjusters.next
 
 class MainActivity : AppCompatActivity() {
+    var button: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btn_next1)
+        button?.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("2주차 숙제")
+                .setMessage("2주차 숙제는 화면전환 예제를 구현하는 내용입니다.")
+                .setPositiveButton("확인") { _, _ -> next() }
+                .create()
+                .show()
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "반가워,", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun next() {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("name", "2주차 숙제 완료!")
+            startActivity(intent)
+
     }
 }
---
+++ yoonwon/app/src/main/java/com/example/bty_homework/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.bty_homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        tv_name.text = intent.getStringExtra("name")
+    }
+}
