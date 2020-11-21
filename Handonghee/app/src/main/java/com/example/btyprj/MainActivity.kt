a3ed9531ff628bef0ee9ff82dd974d00b4a5fc92
Donghee123 <gksehdmgl@gmail.com>
2020-11-22 02:39:44 +0900

    [추가] 3주차 과제

--- Handonghee/app/src/main/java/com/example/btyprj/MainActivity.kt
+++ Handonghee/app/src/main/java/com/example/btyprj/MainActivity.kt
@@ -7,28 +7,37 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
-import java.time.temporal.TemporalAdjusters.next
 
 class MainActivity : AppCompatActivity() {
 
-    var button: Button? = null
-
+    //lateinit var button: Button
+    private val button: Button by lazy{ findViewById<Button>(R.id.btn_2WeekWork)}
+    private val buttonweek3: Button by lazy{ findViewById<Button>(R.id.btn_3WeekWork)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.btn_2WeekWork)
-        button?.setOnClickListener{
+
+        button.setOnClickListener{
             AlertDialog.Builder(this).setTitle("2주차 숙제").setMessage("2주차 숙제는 화면전환 예제를 구현하는 것입니다.").setPositiveButton("확인") {_,_ ->
                 next()
             }.create().show()
         }
 
+        buttonweek3.setOnClickListener{
+            AlertDialog.Builder(this).setTitle("2주차 숙제").setMessage("3주차 숙제는 로그인 화면 예제를 구현하는 것입니다.").setPositiveButton("확인") {_,_ ->
+                OpenWeek3()
+            }.create().show()
+        }
     }
 
     private fun next() {
         var intent = Intent(this, MainActivityWeek2::class.java)
         //intent.putExtra("name", edt_input.text.toString())
+        startActivity(intent)
+    }
+
+    private fun OpenWeek3() {
+        var intent = Intent(this, MainActivityWeek3::class.java)
         startActivity(intent)
     }
 
